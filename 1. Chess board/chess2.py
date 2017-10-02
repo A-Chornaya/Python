074c11009697503d0354fe386ebc57e7a4c47e7d
Anastasiya Chornaya <nastyachornaya4@gmail.com>
2017-10-02 23:06:26 +0300

    Chess board
    Add second file with comand line arguments

---
+++ 1. Chess board/chess2.py
@@ -0,0 +1,33 @@
+import sys
+
+def board(len, hei):
+    s1, s2 = '*', ' '
+    for i in range(1, len):
+        if i % 2 == 0:
+            s1 += '*'
+            s2 += ' '
+        else:
+            s1 += ' '
+            s2 += '*'
+    for j in range(0, hei):
+        if j % 2 == 0:
+            print(s1)
+        else:
+            print(s2)
+
+
+if len(sys.argv) != 3:
+    print(
+        'Invalid number of parameters. Please, start program as: name_of_file length height')
+    sys.exit(1)
+
+try:
+    lenght = int(sys.argv[1])
+    height = int(sys.argv[2])
+    if (lenght <= 0 or height <= 0):
+        raise ValueError
+except ValueError:
+    print('Error of input type. Please, enter a positive integer')
+else:
+    print('======Chess board======')
+    board(lenght, height)
