b3876bbba13637739deeba118bfe411125867c64
Anastasiya Chornaya <nastyachornaya4@gmail.com>
2017-10-09 17:37:36 +0300

    Add tests

---
+++ 2. Envelopes/Envelopes2.py
@@ -0,0 +1,63 @@
+class Envelope:
+    def __init__(self, aa, bb):
+        if aa <= 0 or bb <= 0:
+            raise ValueError
+        self.__a = aa
+        self.__b = bb
+
+    @property
+    def a(self):
+        return self.__a
+
+    @property
+    def b(self):
+        return self.__b
+
+    # this triangle fit to another one
+    def compare_envelopes(self, env):
+        if (((self.a <= env.a) and (self.b <= env.b))
+                or ((self.a <= env.b) and (self.b <= env.a))):
+            return True
+        else:
+            return False
+
+
+def restart_program():
+    answer = input('Do you want to continue? (enter "y" or "yes" if want):')
+    if answer.lower() == 'y' or 'yes' == answer.lower():
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    while True:
+        print('')
+        print('=======Compare envelopes=======')
+        try:
+            a = float(input('Enter side a of Envelope 1:'))
+            b = float(input('Enter side b of Envelope 1:'))
+            c = float(input('Enter side a of Envelope 2:'))
+            d = float(input('Enter side b of Envelope 2:'))
+        except ValueError:
+            print('Error of value types. Sides of the envelopes must be '
+                  'positive numbers')
+            if restart_program():
+                continue
+            else:
+                break
+        else:
+            envelope1 = Envelope(a, b)
+            envelope2 = Envelope(c, d)
+
+            if envelope1.compare_envelopes(envelope2):
+                print('Envelope 1 placed in Envelope 2')
+            elif envelope2.compare_envelopes(envelope1):
+                print('Envelope 2 placed in Envelope 1')
+            else:
+                print('Envelopes don`t fit to each other')
+
+            if restart_program():
+                continue
+            else:
+                break
---
+++ 2. Envelopes/test_envelopes.py
@@ -0,0 +1,40 @@
+import unittest
+from Envelopes2 import *
+
+class TestClassEnvelope(unittest.TestCase):
+    def test_valid_envelope(self):
+        self.env = Envelope(5, 6)
+        self.assertEqual(self.env.a, 5)
+        self.assertEqual(self.env.b, 6)
+
+    def test_except_side_less_zero(self):
+        with self.assertRaises(ValueError):
+            self.env1 = Envelope(-2, 5)
+            self.env2 = Envelope(2, -5)
+            self.env3 = Envelope(-2, -5)
+
+    def test_compare_envelopes(self):
+        self.env1 = Envelope(7, 8)
+        self.env2 = Envelope(3, 4)
+        self.assertFalse(self.env1.compare_envelopes(self.env2))
+        self.assertTrue(self.env2.compare_envelopes(self.env1))
+        self.env1 = Envelope(2, 6)
+        self.env2 = Envelope(4, 1)
+        self.assertFalse(self.env1.compare_envelopes(self.env2))
+        self.assertTrue(self.env2.compare_envelopes(self.env1))
+        self.env1 = Envelope(3, 4)
+        self.env2 = Envelope(5, 7)
+        self.assertTrue(self.env1.compare_envelopes(self.env2))
+        self.assertFalse(self.env2.compare_envelopes(self.env1))
+        self.env1 = Envelope(6, 3)
+        self.env2 = Envelope(4, 9)
+        self.assertTrue(self.env1.compare_envelopes(self.env2))
+        self.assertFalse(self.env2.compare_envelopes(self.env1))
+        self.env1 = Envelope(1, 10)
+        self.env2 = Envelope(2, 5)
+        self.assertFalse(self.env1.compare_envelopes(self.env2))
+        self.assertFalse(self.env2.compare_envelopes(self.env1))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
