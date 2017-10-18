5c1df36d7283c40a8885f7c3c9703b3d515eb1aa
A-Chornaya <anastasiyachorna@mail.ru>
2017-10-19 01:34:57 +0300

    change page orders with users and dishes

--- Py_Project/sushi/sushi_rinjin/views/view_order.py
+++ Py_Project/sushi/sushi_rinjin/views/view_order.py
@@ -3,19 +3,15 @@ from sushi_rinjin.models.order import Order
 from sushi_rinjin.models.order_list import OrderList
 
 
-
 def index(request):
     orders = Order.objects.all()
-    order_list_list = list()
-    #result_list = list()
+    result_list = list()
+
     for ord in orders:
+        user = ord.user_id.user_name
         list_order = list()
         for obj_list in OrderList.objects.filter(order_id=ord.id):
             list_order.append(obj_list.dish_id.dish_name)
-        order_list_list.append(list_order)
-        #result_list.append([ord.id, ])
-    context = {'orders': [(order.id, Order.objects.filter(
-                   id=order.id).values('id', 'user_id__user_name'))
-                          for order in Order.objects.all()],
-               'order_list_list': order_list_list}
+        result_list.append([ord.id, user, list_order])
+    context = {'result_list': result_list}
     return render(request, 'sushi_rinjin/orders.html', context)
