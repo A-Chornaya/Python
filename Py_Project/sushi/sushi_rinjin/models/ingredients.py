ce787067876a1e98cb52804e709be37da2dbd419
A-Chornaya <anastasiyachorna@mail.ru>
2017-10-24 13:53:20 +0300

    Add form to add ingredients

--- Py_Project/sushi/sushi_rinjin/models/ingredients.py
+++ Py_Project/sushi/sushi_rinjin/models/ingredients.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.forms import ModelForm
 
 
 class Ingredients(models.Model):
-    ingredient = models.CharField(max_length=50)
+    ingredient = models.CharField(max_length=50, unique=True)
 
-    @property
     def __str__(self):
         return self.ingredient
+
+
+class IngredientsForm(ModelForm):
+    class Meta:
+        model = Ingredients
+        fields = ['ingredient']
